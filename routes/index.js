3147f13b62d80b0ac1a16ed4af8dfa724cf0bf1d
vierukundo <vierukundo@gmail.com>
2024-03-13 05:38:37 -0700

    fixed code to pass eslint

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 // routes/index.js
-const express = require("express");
-const AppController = require("../controllers/AppController");
-const AuthController = require("../controllers/AuthController");
-const UsersController = require("../controllers/UsersController");
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const AuthController = require('../controllers/AuthController');
+const UsersController = require('../controllers/UsersController');
+
 const router = express.Router();
 
-router.get("/status", AppController.getStatus);
-router.get("/stats", AppController.getStats);
-router.get("/connect", AuthController.getConnect);
-router.get("/disconnect", AuthController.getDisconnect);
-router.get("/users/me", UsersController.getMe);
-//router.get("/files/:id", FilesController.getShow);
-//router.get("/files", FilesController.getIndex);
-router.post("/users", UsersController.postNew);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+// router.get("/files/:id", FilesController.getShow);
+// router.get("/files", FilesController.getIndex);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
